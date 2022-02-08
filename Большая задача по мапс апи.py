e843c09ddce8c89e58bc9e21fad3a05d37f73396
1234 <1234>
2022-02-08 21:17:41 +0300

    Задача 3

--- Большая задача по мапс апи.py
+++ Большая задача по мапс апи.py
@@ -1,10 +1,8 @@
 import os
 import sys
 import requests
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
-from check_coord import check_spn as chk_spn
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.Qt import *
 
 SCREEN_SIZE = [600, 450]
 coords = "37.404304, 55.652923"
@@ -17,12 +15,14 @@ class Example(QWidget):
         self.coords = coords.replace(' ', '')
         self.spn_input = spn
         self.spn = self.spn_input + ',' + '' + self.spn_input
+        self.coords_x, self.coords_y = self.coords.split(',')
         self.image = QLabel(self)
         self.image.move(0, 0)
         self.image.resize(600, 450)
         self.getImage()
 
     def keyPressEvent(self, event):
+        self.coords_x, self.coords_y = self.coords.split(',')
         if event.key() == Qt.Key_PageDown:
             if float(self.spn_input) < 40:
                 self.spn_input = str(float(self.spn_input) + 0.6)[:5]
@@ -35,12 +35,53 @@ class Example(QWidget):
                 self.spn_input = str(float(self.spn_input) - 0.02)[:5]
             if float(self.spn_input) > 0.0021 and float(self.spn_input) < 0.02:
                 self.spn_input = str(float(self.spn_input) - 0.002)[:5]
+        if event.key() == Qt.Key_Up:
+            if float(self.coords_y) < 84:
+                if float(self.spn_input) > 1:
+                    self.coords_y = float(float(self.coords_y) + 0.5)
+                if float(self.spn_input) > 0.3 and float(self.spn_input) <= 1:
+                    self.coords_y = float(float(self.coords_y) + 0.05)
+                if float(self.spn_input) > 0.05 and float(self.spn_input) <= 0.3:
+                    self.coords_y = float(float(self.coords_y) + 0.03)
+                if float(self.spn_input) > 0.001 and float(self.spn_input) <= 0.05:
+                    self.coords_y = float(float(self.coords_y) + 0.001)
+        if event.key() == Qt.Key_Down:
+            if float(self.coords_y) > -84:
+                if float(self.spn_input) > 1:
+                    self.coords_y = float(float(self.coords_y) - 0.5)
+                if float(self.spn_input) > 0.3 and float(self.spn_input) <= 1:
+                    self.coords_y = float(float(self.coords_y) - 0.05)
+                if float(self.spn_input) > 0.05 and float(self.spn_input) <= 0.3:
+                    self.coords_y = float(float(self.coords_y) - 0.03)
+                if float(self.spn_input) > 0.001 and float(self.spn_input) <= 0.05:
+                    self.coords_y = float(float(self.coords_y) - 0.001)
+        if event.key() == Qt.Key_Right:
+            if float(self.coords_x) < 179.99:
+                if float(self.spn_input) > 1:
+                    self.coords_x = float(float(self.coords_x) + 0.5)
+                if float(self.spn_input) > 0.3 and float(self.spn_input) <= 1:
+                    self.coords_x = float(float(self.coords_x) + 0.05)
+                if float(self.spn_input) > 0.05 and float(self.spn_input) <= 0.3:
+                    self.coords_x = float(float(self.coords_x) + 0.03)
+                if float(self.spn_input) > 0.001 and float(self.spn_input) <= 0.05:
+                    self.coords_x = float(float(self.coords_x) + 0.001)
+        if event.key() == QtCore.Qt.Key_Left:
+            if float(self.coords_x) > -179.99:
+                if float(self.spn_input) > 1:
+                    self.coords_x = float(float(self.coords_x) - 0.5)
+                if float(self.spn_input) > 0.3 and float(self.spn_input) <= 1:
+                    self.coords_x = float(float(self.coords_x) - 0.05)
+                if float(self.spn_input) > 0.05 and float(self.spn_input) <= 0.3:
+                    self.coords_x = float(float(self.coords_x) - 0.03)
+                if float(self.spn_input) > 0.001 and float(self.spn_input) <= 0.05:
+                    self.coords_x = float(float(self.coords_x) - 0.001)
         self.getImage()
 
     def getImage(self):
         map_api_server = "http://static-maps.yandex.ru/1.x/"
         self.spn = self.spn_input + ',' + '' + self.spn_input
-        print(self.spn)
+        self.coords = str(self.coords_x) + ',' + str(self.coords_y)
+
         map_params = {
             "ll": self.coords,
             "spn": self.spn,
